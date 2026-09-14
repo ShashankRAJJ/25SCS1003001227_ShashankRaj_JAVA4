980787a418ab186fef6eaeee57c69ec63e65ab1f
ShashankRaj <shashank.25scs1003001227@iilm.edu>
2026-09-14 14:30:36 +0530

    Add files via upload

---
+++ JAVA/Assignment_6/Q_01.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Q_01 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum = sum + arr[i];
+        }
+
+        System.out.println("Sum of array elements: " + sum);
+
+        sc.close();
+    }
+}
---
+++ JAVA/Assignment_6/Q_02.java
@@ -0,0 +1,35 @@
+// Q2. Find Largest Element in an Array
+
+import java.util.Scanner;
+
+public class Q_02 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest element: " + largest);
+
+        sc.close();
+    }
+}
+
